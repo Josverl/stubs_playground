3f983ee827abfc51c38edf67389104fdc9ddfe28
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-09-03 20:12:08 +0200

    chore: update package versions for lsp-client and pyright-worker to 0.3.6 and 0.4.6 respectively
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

--- apps/playground/component-config.generated.js
+++ apps/playground/component-config.generated.js
@@ -2,12 +2,12 @@
 export const componentConfig = Object.freeze({
     "lspClient": {
         "packageName": "@mp-typing/lsp-client",
-        "version": "0.3.5",
+        "version": "0.3.6",
         "entry": "src/index.js"
     },
     "pyrightWorker": {
         "packageName": "@mp-typing/pyright-worker",
-        "version": "0.4.5",
+        "version": "0.4.6",
         "worker": "dist/pyright_worker.js",
         "assets": "assets"
     }
